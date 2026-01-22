7e3e175a22a4045e689d09cc1a114ce6cee599d5
ARoskelley <aroskelley2112@gmail.com>
2026-01-21 23:05:58 -0700

    a

---
+++ test.py
@@ -0,0 +1,9 @@
+color = "blue"
+print(f"your favorite color is {color}.")
+food = "chinese food"
+print(f"your favorite food is {food}.")
+color = input("please type your favorite color: ")
+food = input("please type your favorite food: ")
+print(f"your favorite color is {color} and your favorite food is {food}!")
+sport = input("please type your favorite sport: ")
+print(f"your favorite sport is {sport}.")
